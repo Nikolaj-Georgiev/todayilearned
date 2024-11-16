01f482905671bd2c447c8fed36b11f2620afdfc3
Nikolay Georgiev <47087200+Nikolaj-Georgiev@users.noreply.github.com>
2024-11-17 01:06:34 +0200

    fixed the supabase key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sqghliaxjbqxyapstzaf.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
